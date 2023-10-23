7c3fc11e7d760dd621a43049253c26b7fccf96e3
kill-your-soul <Platonovdi@gmail.com>
2023-10-23 23:13:50 +0300

    Start of translation to other gui framework

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use winit::{
-    event::{Event, WindowEvent},
+    event::{Event, WindowEvent, KeyEvent, ElementState},
     event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder
+    window::{WindowBuilder, WindowButtons},
+    keyboard::Key
 };
+
+#[path = "util/fill.rs"]
+mod fill;
 
 fn main() {
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-    window.set_title("Rustpad");
+    let window = WindowBuilder::new()
+        .with_title("Rustpad")
+        .with_decorations(true)
+        .build(&event_loop)
+        .unwrap();
+    
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
         match event {
@@ -15,6 +23,37 @@ fn main() {
                 event: WindowEvent::CloseRequested,
                 window_id
             } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            // Event::DeviceEvent { event: WindowEvent::KeyboardInput { device_id, event, is_synthetic } }
+            Event::WindowEvent {
+                event:
+                    WindowEvent::KeyboardInput {
+                        event:
+                            KeyEvent {
+                                logical_key: key,
+                                state: ElementState::Pressed,
+                                ..
+                            },
+                        ..
+                    },
+                ..
+            } => match key.as_ref() {
+                Key::Character("F" | "f") => {
+                    let buttons = window.enabled_buttons();
+                    window.set_enabled_buttons(buttons ^ WindowButtons::CLOSE);
+                }
+                Key::Character("G" | "g") => {
+                    let buttons = window.enabled_buttons();
+                    window.set_enabled_buttons(buttons ^ WindowButtons::MAXIMIZE);
+                }
+                Key::Character("H" | "h") => {
+                    let buttons = window.enabled_buttons();
+                    window.set_enabled_buttons(buttons ^ WindowButtons::MINIMIZE);
+                }
+                _ => (),
+            },
+            Event::RedrawRequested(_) => {
+                fill::fill_window(&window, 0xe575b8);
+            },
             _ => (),
         }
     });
